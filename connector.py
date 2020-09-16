0c54ece68b9e8a951c1f3818d44508895ee511df
Choolwe <34605051+chomula@users.noreply.github.com>
2020-09-16 15:01:15 +0200

    Connection to DB plus Query
    Query to create DB

--- connector.py
+++ connector.py
@@ -1,1 +1,11 @@
+import mysql.connector
 
+MyDB = mysql.connector.connect(
+host = "localhost",
+user = "root",
+password = "",
+)
+
+mycursor =  MyDB.cursor()
+
+mycursor.execute('CREATE DATABASE gradingSysDB')
