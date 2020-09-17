1dfde908be2773e485ffb9bc1b76f043a8120214
Choolwe <34605051+chomula@users.noreply.github.com>
2020-09-17 09:56:27 +0200

    added queries for table creation
    Two tables created in DB

--- connector.py
+++ connector.py
@@ -9,3 +9,6 @@ password = "",
 mycursor =  MyDB.cursor()
 
 mycursor.execute('CREATE DATABASE gradingSysDB')
+
+mycursor.execute('CREATE TABLE student( S_id INT AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(120), lname VARCHAR(120), grade VARCHAR(30), class VARCHAR(30)')
+mycursor.execute('CREATE TABLE teacher( T_id INT AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(120), lname VARCHAR(120), teacher_id VARCHAR(30), class VARCHAR(30), passwd VARCHAR(120)')
